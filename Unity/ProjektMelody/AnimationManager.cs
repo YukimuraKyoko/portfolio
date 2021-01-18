3841b73b62849dcfbadadaef731919f7d0b2e715
Tina <TinaNguyen290@yahoo.com>
2021-01-17 17:44:07 -0800

    Added scripts since I've gotten permission from DigiTrevX

---
+++ Unity/ProjektMelody/AnimationManager.cs
@@ -0,0 +1,197 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MorphAnimation
+{
+    string name;
+    
+    SkinnedMeshRenderer renderer;
+    int blendShapeIdx;
+    float startingValue;
+    float direction = 1;
+    bool updating = false;
+    float increment = 15f;
+    //Unused float MaxValue = 100f;
+    //Unused float MinValue = 0f;
+    public MorphAnimation(string n, SkinnedMeshRenderer r)
+    {
+        name = n;
+        renderer = r;
+        blendShapeIdx = r.sharedMesh.GetBlendShapeIndex(n);
+        startingValue = r.GetBlendShapeWeight(blendShapeIdx);
+    }
+
+    public void Trigger()
+    {
+        updating = true;
+        Debug.LogFormat("Triggering facemorph {0} in {1} direction.", name, (direction > 0) ? "positive" : "negative");
+    }
+
+    public void Update()
+    {
+        if (!updating)
+            return;
+
+        bool complete = false;
+        float newVal = renderer.GetBlendShapeWeight(blendShapeIdx) + (direction * increment);
+        if (newVal <= 0)
+        {
+            newVal = 0;
+            complete = true;
+        } else if (newVal >= 100f)
+        {
+            newVal = 100;
+            complete = true;
+        }
+        renderer.SetBlendShapeWeight(blendShapeIdx, newVal);
+
+        if (complete)
+        {
+            updating = false;
+            direction *= -1;
+        }
+    }
+}
+
+/*
+ * //Class array approach didn't work
+ * //Reason: In the editor script it kept having an error:
+ * "Object Reference not set to an instance of an object"
+[System.Serializable]
+ public class PairElement
+{
+    public string param;
+    public KeyCode key;
+
+    public PairElement()
+    {
+
+    }
+}
+*/
+
+public class AnimationManager : MonoBehaviour
+{
+    public Animator animator;
+    public string[] temp;
+    public KeyCode[] keys;
+    public string[] parameters;
+
+    public Dictionary<KeyCode, string> animationMap = new Dictionary<KeyCode, string>() {};
+
+    Dictionary<KeyCode, string> morphShapeMap = new Dictionary<KeyCode, string>()
+    {
+        {KeyCode.J, "Ahegao Yeah"},
+        {KeyCode.K, "TongueOut_Ahhh"},
+        {KeyCode.H, "Heart Eyes"}
+    };
+
+    Dictionary<string, MorphAnimation> morphAnimationMap = new Dictionary<string, MorphAnimation>();
+
+    // Last jacket var
+    bool lastJacket = true;
+
+    void Awake()
+    {
+        
+        // check for conflicting keybindings
+        var renderer = GameObject.Find("Body").GetComponent<SkinnedMeshRenderer>();
+        if (renderer == null)
+        {
+            Debug.LogError("AnimationManager could not find skinned mesh renderer. Can't trigger face morphs.");
+        }
+        else
+        {
+            foreach (string morphName in morphShapeMap.Values)
+            {
+                morphAnimationMap.Add(morphName, new MorphAnimation(morphName, renderer));
+            }
+        }
+    }
+
+    // Use this for initialization		
+    void Start()
+    {
+        if (animator == null)
+        {
+            Debug.LogError("No animator found. Is it set in the inspector?");
+        }
+
+        //Initialize parameter array into the animationMap on Start
+        for (int i = 0; i < parameters.Length; i++)
+        {
+            animationMap.Add(keys[i], parameters[i]);
+
+            //Debug animationMap
+            //Debug.Log("keys["+i+"] = " + keys[i] + " parameters["+i+"] = " + parameters[i]);
+        }
+    }
+
+    void OnGUI()
+    {
+        if (animator == null)
+        {
+            return;
+        }
+           
+        if (animator.parameterCount == 0)
+        {
+            Debug.LogError("Could not retrieve parameters in the selected Animator!");
+        }
+       
+
+        
+
+        if (Event.current.isKey && Event.current.type == EventType.KeyDown)
+        {
+            var pressedKey = Event.current.keyCode;
+            if (animationMap.ContainsKey(pressedKey))
+            {
+                string animationTriggered = animationMap[pressedKey];
+                bool currentValue = animator.GetBool(animationTriggered);
+
+                if (animator.runtimeAnimatorController != null)
+                {
+                    animator.SetBool(animationTriggered, !currentValue);
+                }
+                    
+            } else if (morphShapeMap.ContainsKey(pressedKey))
+            {
+                string morphTriggered = morphShapeMap[pressedKey];
+                morphAnimationMap[morphTriggered].Trigger();
+            }
+        }
+
+        
+    }
+
+    void Update()
+    {
+        foreach (MorphAnimation ma in morphAnimationMap.Values)
+        {
+            ma.Update();
+        }
+    }
+
+    public void SetOnElgatoTriggered()
+    {
+        //Connecting Elgato
+        ElgatoInputSystem.Instance._onTriggerAnimation = OnElgatoTriggered;
+    }
+
+    private void OnElgatoTriggered(int keyCode, bool isPressed)
+    {
+        KeyCode pressedKey = (KeyCode)keyCode;
+        if (animationMap.ContainsKey(pressedKey))
+        {
+            string animationTriggered = animationMap[pressedKey];
+            animator.SetBool(animationTriggered, isPressed);
+        }
+        else if (morphShapeMap.ContainsKey(pressedKey))
+        {
+            string morphTriggered = morphShapeMap[pressedKey];
+            morphAnimationMap[morphTriggered].Trigger();
+        }
+    }
+}
---
+++ Unity/ProjektMelody/AnimationManagerEditor.cs
@@ -0,0 +1,248 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Text.RegularExpressions; // needed for Regex
+
+[CustomEditor(typeof(AnimationManager))]
+public class AnimationManagerEditor : Editor
+{
+
+    AnimationManager AM;
+    
+    SerializedProperty temp;
+    SerializedProperty anim;
+    SerializedProperty param;
+    SerializedProperty keys;
+
+    bool manualSettings = false;
+    bool simpleSettings = false;
+
+    void OnEnable()
+    {
+
+        temp = serializedObject.FindProperty("temp"); //string[] temp;
+        anim = serializedObject.FindProperty("animator"); //Animator animator;
+        param = serializedObject.FindProperty("parameters"); //string[] parameters;
+        keys = serializedObject.FindProperty("keys"); //KeyCode[] keys;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        EditorGUI.indentLevel = 0;
+        serializedObject.Update();
+        EditorGUILayout.Space();
+        //base.OnInspectorGUI();
+        AM = (AnimationManager)target;
+
+        EditorGUILayout.BeginHorizontal();
+        if (GUILayout.Button("Generate Parameters", GUILayout.Height(20)))
+        {
+            //Uncomment line below to bring up old version of the animationkeybind window
+            //AnimationKeyBind.OpenWindow(AM);
+
+
+            temp.arraySize = AM.animator.parameterCount;
+            param.arraySize = AM.animator.parameterCount;
+            //Debug.Log(param.arraySize);
+            //Fill and display parameters
+            for (int i = 0; i < AM.parameters.Length; i++)
+            {
+                param.GetArrayElementAtIndex(i).stringValue = AM.animator.parameters[i].name;
+
+            }
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        if (GUILayout.Button("Reset Parameters", GUILayout.Height(20)))
+        {
+            
+            AM.temp = new string[AM.animator.parameterCount];
+            AM.parameters = new string[AM.animator.parameterCount];
+            param.arraySize = AM.parameters.Length;
+            serializedObject.ApplyModifiedProperties();
+
+            Debug.Log("AM.temp.length: " + AM.temp.Length + "\n" + 
+                "AM.parameters.length: " + AM.parameters.Length); 
+        }
+
+        if(GUILayout.Button("Reset Keys", GUILayout.Height(20)))
+        {
+            AM.keys = new KeyCode[AM.animator.parameterCount];
+            keys.arraySize = AM.keys.Length;
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        EditorGUILayout.EndHorizontal();
+
+        if (GUILayout.Button("Open Settings", GUILayout.Height(20)))
+        {
+            manualSettings = !manualSettings;
+        }
+        /*
+        if (GUILayout.Button("Simple Settings", GUILayout.Height(20)))
+        {
+            simpleSettings = !simpleSettings;
+        }
+        */
+        EditorGUILayout.Space();
+        
+
+        //Show and Apply Animator Input box
+        EditorGUILayout.BeginHorizontal();
+        EditorGUI.BeginChangeCheck();
+        EditorGUILayout.PropertyField(anim, false);
+        EditorGUI.EndChangeCheck();
+        serializedObject.ApplyModifiedProperties();
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.HelpBox("Temporary Note: \n" +
+            "Currently this manager works only if each parameter is assigned to only 1 unique button. \n" +
+            "Eg. Can't have Two 'A' buttons in the slots. \n" +
+            "When using another Animator in the slot above, click Reset Parameters, Reset Keys, and then Generate Parameters. \n" +
+            "\n" +
+            "The keys must be consistently assigned from top to bottom, can't leave any of them as none.", MessageType.Info);
+
+
+
+        if (manualSettings)
+        {
+            DisplayDoubleArrays();
+        }
+        /*
+        if (simpleSettings)
+        {
+            DisplayAllKeyInputBoxes();
+        }
+        */
+
+       
+
+    }
+
+
+
+
+
+    void DisplayAllKeyInputBoxes()
+    {
+        for (int i = 0; i < AM.temp.Length; i++)
+        {
+            EditorGUILayout.BeginHorizontal();
+
+            EditorGUI.BeginChangeCheck();
+
+            //Display Key input box
+            AM.temp[i] = EditorGUILayout.TextField(AM.temp[i], GUILayout.Width(30));
+
+            //Force input string to the first character
+            if (AM.temp[i].Length > 1)
+            {
+                AM.temp[i] = AM.temp[i].Substring(0, 1);
+            }
+
+            //Force input string to only be Uppercase
+            if (AM.temp[i] != AM.temp[i].ToUpper())
+            {
+                AM.temp[i] = AM.temp[i].ToUpper();
+            }
+
+
+            EditorGUI.EndChangeCheck();
+            //Restrict Key input box to only A-Z, 0-9 keys
+            AM.temp[i] = Regex.Replace(AM.temp[i], @"[^A-Z0-9]", "");
+            temp.GetArrayElementAtIndex(i).stringValue = AM.temp[i];
+            AM.keys[i] = (KeyCode)System.Enum.Parse(typeof(KeyCode), AM.temp[i]);
+
+
+            serializedObject.ApplyModifiedProperties();
+            //Display Parameter name
+            GUILayout.Label(AM.parameters[i]);
+            EditorGUILayout.EndHorizontal();
+        }
+    }
+
+
+
+    void DisplayDoubleArrays()
+    {
+        EditorGUILayout.BeginHorizontal();
+
+        EditorGUI.BeginChangeCheck();
+            EditorGUILayout.BeginVertical();
+            for (int i = 0; i <param.arraySize; i++)
+            {
+                
+                EditorGUILayout.PropertyField(param.GetArrayElementAtIndex(i), GUIContent.none);
+            }
+            EditorGUILayout.EndVertical();
+
+
+        EditorGUILayout.BeginVertical();
+        for (int i = 0; i < keys.arraySize; i++)
+        {
+
+            EditorGUILayout.PropertyField(keys.GetArrayElementAtIndex(i), GUIContent.none);
+        }
+        EditorGUILayout.EndVertical();
+
+
+        if (EditorGUI.EndChangeCheck())
+        {
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        EditorGUILayout.EndHorizontal();
+    }
+
+}
+
+
+
+//Unused for now
+public class AnimationKeyBind : EditorWindow
+{
+    
+    
+    static AnimationKeyBind window;
+    static AnimationManager AM2;
+    public static void OpenWindow(AnimationManager AM)
+    {
+        AM2 = AM;
+        window = (AnimationKeyBind)GetWindow(typeof(AnimationKeyBind));
+        window.minSize = new Vector2(400, 600);
+        window.Show();
+    }
+
+    void OnGUI()
+    {
+        //Source Animator Input
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.Label("Animator");
+        AM2.animator = (Animator)EditorGUILayout.ObjectField(AM2.animator, typeof(Animator), true);
+        EditorGUILayout.EndHorizontal();
+
+        if (GUILayout.Button("Generate Parameters", GUILayout.Height(20)))
+        {
+            GenerateParameters();
+
+        }
+
+        //Debug.Log(AM2.parameters[0]);
+    }
+
+
+    void GenerateParameters()
+    {
+        for (int i = 0; i < AM2.parameters.Length; i++)
+        {
+            EditorGUILayout.BeginHorizontal();
+            GUILayout.Label(AM2.parameters[i]);
+            //AM2.animator = (Animator)EditorGUILayout.ObjectField(AM2.animator, typeof(Animator), true);
+            EditorGUILayout.EndHorizontal();
+        }
+    }
+
+
+
+}
